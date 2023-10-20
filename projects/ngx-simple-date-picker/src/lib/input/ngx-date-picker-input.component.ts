9b6a6df4dcd842bf65375d6b9191cecba043357f
e73b025 <60853824+e73b025@users.noreply.github.com>
2023-10-20 18:11:37 +0100

    Small fixes and improvements.

--- projects/ngx-simple-date-picker/src/lib/input/ngx-date-picker-input.component.ts
+++ projects/ngx-simple-date-picker/src/lib/input/ngx-date-picker-input.component.ts
@@ -92,6 +92,24 @@ export class NgxDatePickerInputComponent implements OnInit {
     return datePipe.transform(this.date?.startDate, this.config.datePresentationFormat)
       + ' - ' + datePipe.transform(this.date?.endDate, this.config.datePresentationFormat);
   }
+
+  getCalendarIcon(): string {
+    if (this.config.calendarIcon) {
+      return this.config.calendarIcon;
+    }
+
+    return 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCEtLSBHZW5lcmF0b3I6IEFkb2' +
+      'JlIElsbHVzdHJhdG9yIDI4LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPgo8c3ZnIHZ' +
+      'lcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93' +
+      'd3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IgoJIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3Jv' +
+      'dW5kOm5ldyAwIDAgMjQgMjQ7IiB4bWw6c3BhY2U9InByZXNlcnZlIj4KPHN0eWxlIHR5cGU9InRleHQvY3NzIj4KCS5zdDB7ZmlsbDojN0M3Q' +
+      'zdDO30KPC9zdHlsZT4KPHBhdGggY2xhc3M9InN0MCIgZD0iTTE0LjUsMThjLTAuNywwLTEuMy0wLjItMS44LTAuN2MtMC41LTAuNS0wLjctMS' +
+      '4xLTAuNy0xLjhjMC0wLjcsMC4yLTEuMywwLjctMS44YzAuNS0wLjUsMS4xLTAuNywxLjgtMC43CglzMS4zLDAuMiwxLjgsMC43YzAuNSwwLjU' +
+      'sMC43LDEuMSwwLjcsMS44YzAsMC43LTAuMiwxLjMtMC43LDEuOEMxNS44LDE3LjgsMTUuMiwxOCwxNC41LDE4eiBNNSwyMmMtMC41LDAtMS0w' +
+      'LjItMS40LTAuNgoJUzMsMjAuNSwzLDIwVjZjMC0wLjUsMC4yLTEsMC42LTEuNEM0LDQuMiw0LjUsNCw1LDRoMVYyaDJ2Mmg4VjJoMnYyaDFjM' +
+      'C42LDAsMSwwLjIsMS40LDAuNkMyMC44LDUsMjEsNS40LDIxLDZ2MTQKCWMwLDAuNS0wLjIsMS0wLjYsMS40UzE5LjYsMjIsMTksMjJINXogTT' +
+      'UsMjBoMTRWMTBINVYyMHogTTUsOGgxNFY2SDVWOHogTTUsOFY2Vjh6Ii8+Cjwvc3ZnPgo=';
+  }
 }
 
 /**
@@ -101,6 +119,7 @@ export interface NgxDatePickerInputConfig extends NgxDatePickerConfig {
   datePresentationFormat: string;
   datePresentationLocale: string;
   datePresentationValue: any;
+  calendarIcon: any
 }
 
 /**
@@ -112,6 +131,7 @@ export const getInputConfigDefaults = (): NgxDatePickerInputConfig => {
   return Object.assign(getConfigDefaults(), {
     datePresentationFormat: 'MMM d, y',
     datePresentationLocale: 'en-US',
-    datePresentationValue: 'Click Here to Choose Date'
+    datePresentationValue: 'Click Here to Choose Date',
+    calendarIcon: undefined
   });
 }
